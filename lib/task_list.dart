25ceedde2133c3604a8258d038cb4ceadd5c22ad
eidan <acondori@netactica.com>
2023-09-17 18:14:02 -0400

    task list custom row

--- lib/task_list.dart
+++ lib/task_list.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class TaskList extends StatelessWidget {
   final List<String> tasks;
 
-  TaskList(this.tasks, {super.key});
+  const TaskList(this.tasks, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,5 +15,43 @@ class TaskList extends StatelessWidget {
         );
       },
     );
+  }
+
+  Widget getDetail(int index, String name) {
+    final title = Container(
+      margin: const EdgeInsets.only(
+        top: 5,
+        left: 10,
+      ),
+      child: Text(
+        "$index :",
+        style: const TextStyle(
+          fontFamily: "Roboto",
+          fontSize: 18,
+          fontWeight: FontWeight.w900,
+          color: Colors.black,
+        ),
+      ),
+    );
+    final detail = Container(
+      margin: const EdgeInsets.only(top: 0, left: 10, bottom: 25),
+      child: Text(
+        name,
+        style: const TextStyle(
+          fontFamily: "Roboto",
+          fontSize: 16,
+          fontWeight: FontWeight.w300,
+          color: Color.fromARGB(255, 0, 0, 0),
+        ),
+      ),
+    );
+    final info = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        title,
+        detail,
+      ],
+    );
+    return info;
   }
 }
